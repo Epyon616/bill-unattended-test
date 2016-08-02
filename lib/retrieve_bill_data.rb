e9a3b705ccbee95b6ef0e4193f8b5e9199e0a873
Lee Richmond <lee@stealthygecko.com>
2016-08-03 00:34:52 +0100

    Keep coding style consistent
    I realised I wasn't being consistent with using metaclasses to make the code more readable

--- lib/retrieve_bill_data.rb
+++ lib/retrieve_bill_data.rb
@@ -1,8 +1,10 @@
 require 'http'
 
 class RetrieveBillData
-  def self.call
-    data_url = HTTP.get("http://safe-plains-5453.herokuapp.com/bill.json")
-    JSON.parse(data_url)
+  class << self
+    def call
+      data_url = HTTP.get("http://safe-plains-5453.herokuapp.com/bill.json")
+      JSON.parse(data_url)
+    end
   end
 end
