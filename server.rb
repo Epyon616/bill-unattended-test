06beb1c315ad64a794685edff9c0600d1357fdf0
Lee Richmond <lee@stealthygecko.com>
2016-08-02 23:12:01 +0100

    update server file
    * Update server file to serve actual content
    * Add Date library for date and time parsing

--- server.rb
+++ server.rb
@@ -1,7 +1,13 @@
 require 'sinatra'
+require 'sinatra/static_assets'
+require 'date'
+require_relative "./bill"
 
 class Server < Sinatra::Base
+  register Sinatra::StaticAssets
+
   get "/" do
-    "Lets get the party started!"
+    @bill = Bill.new
+    erb :bill
   end
 end
