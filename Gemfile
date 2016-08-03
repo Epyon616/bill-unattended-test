43eb5e15d92175faad66cf3acb695de7a48d59e8
Lee Richmond <lee@stealthygecko.com>
2016-08-03 19:32:37 +0100

    remove need for Sinatra static assets

--- Gemfile
+++ Gemfile
@@ -5,7 +5,6 @@ ruby '2.3.0'
 gem 'sinatra'
 gem 'http'
 gem 'json'
-gem 'sinatra-static-assets'
 
 group :test do
   gem 'rspec'
--- server.rb
+++ server.rb
@@ -1,10 +1,11 @@
-require 'sinatra'
-require 'sinatra/static_assets'
+require 'sinatra/base'
 require 'date'
 require_relative "./bill"
+require_relative './views/helpers/view_helpers'
 
 class Server < Sinatra::Base
-  register Sinatra::StaticAssets
+  include ViewHelpers
+  set :public_folder, File.dirname(__FILE__) + '/public'
 
   get "/" do
     @bill = Bill.new
